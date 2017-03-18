97f4786ca993087c5a5bd9eac3771abd5f9d16f9
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-18 14:02:49 +0400

    верстка приведена в порядок, теперь mobile-first

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import React,{Component} from 'react';
 export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-			  <header className="header">
+      <div className="App">
+        <header className="header">
           <div className="content">
             <div className="header__group">
               <div className="logo">
@@ -13,7 +13,7 @@ export default class App extends Component {
               <div className="sign-in">
                 <a href="#" className="sign-in__link">Войти</a>
               </div>
-           </div>
+            </div>
           </div>
         </header>
         <nav className="navigation">
@@ -21,107 +21,63 @@ export default class App extends Component {
           <a className="navigation__link">Практикум</a>
           <a className="navigation__link">Прогресс</a>
         </nav>
-        <div className="content mission">
-          <h2><a href="#" className="mission__text">Начать миссию</a></h2>
+        <div className="mission">
+          <a href="#" className="mission__text">Начать миссию</a>
         </div>
         <main className="main content">
-          <section className="section fr">
-            <div className="subject__icon"></div>
-            <div className="subject fc-1">
-              <h3 className="subject__title fc-11"><a href="">Проценты, пропорции и дроби</a></h3>
-              <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+          <section className="section subject">
+            <div className="subject__group">
+              <div className="subject__icon"></div>
+              <a href="#" className="subject__title">Проценты, пропорции и дроби</a>
             </div>
-            <aside className="lessons fc-1 fr">
-              <ul className="lessons__list">
-                <li><a href="#">Введние в проценты</a></li>
-                <li><a href="#">Введение в дроби</a></li>
-                <li><a href="#">Сложение дробей</a></li>
-              </ul>
-              <ul className="lessons__list">
-                <li><a href="#">Введние в проценты</a></li>
-                <li><a href="#">Введение в дроби</a></li>
-                <li><a href="#">Сложение дробей</a></li>
-              </ul>
-            </aside>
-          </section>
-          <section className="section fr">
-            <div className="subject__icon"></div>
-            <div className="subject fc-4">
-              <h3 className="subject__title fc-11"><a href="">Проценты, пропорции и дроби</a></h3>
-              <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+            <div className="subject__container">
+              <p className="subject__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+              <div className="subject__lessons">
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+              </div>
             </div>
-            <aside className="lessons fc-4 fr">
-              <ul className="lessons__list">
-                <li><a href="#">Введние в проценты</a></li>
-                <li><a href="#">Введение в дроби</a></li>
-                <li><a href="#">Сложение дробей</a></li>
-              </ul>
-              <ul className="lessons__list">
-              <li><a href="#">Введние в проценты</a></li>
-              <li><a href="#">Введение в дроби</a></li>
-              <li><a href="#">Сложение дробей</a></li>
-              </ul>
-            </aside>
           </section>
-          <section className="section fr">
-            <div className="subject__icon"></div>
-            <div className="subject fc-4">
-              <h3 className="subject__title fc-11"><a href="">Проценты, пропорции и дроби</a></h3>
-              <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+          <section className="section subject">
+            <div className="subject__group">
+              <div className="subject__icon"></div>
+              <a href="#" className="subject__title">Проценты, пропорции и дроби</a>
             </div>
-            <aside className="lessons fc-4 fr">
-              <ul className="lessons__list">
-                <li><a href="#">Введние в проценты</a></li>
-                <li><a href="#">Введение в дроби</a></li>
-                <li><a href="#">Сложение дробей</a></li>
-              </ul>
-              <ul className="lessons__list">
-                <li><a href="#">Введние в проценты</a></li>
-                <li><a href="#">Введение в дроби</a></li>
-                <li><a href="#">Сложение дробей</a></li>
-              </ul>
-            </aside>
+            <div className="subject__container">
+              <p className="subject__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+              <div className="subject__lessons">
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+              </div>
+            </div>
           </section>
-          {/*<section className="section fr">
-                     <div className="subject__icon"></div>
-                     <div className="subject fc-4">
-                      <h3 className="subject__title fc-11"><a href="">Проценты, пропорции и дроби</a></h3>
-                      <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
-                    </div>
-                    <aside className="lessons fc-4 fr">
-                      <ul className="lessons__list">
-                        <li><a href="#">Введние в проценты</a></li>
-                        <li><a href="#">Введение в дроби</a></li>
-                        <li><a href="#">Сложение дробей</a></li>
-                      </ul>
-                      <ul className="lessons__list">
-                        <li><a href="#">Введние в проценты</a></li>
-                        <li><a href="#">Введение в дроби</a></li>
-                        <li><a href="#">Сложение дробей</a></li>
-                      </ul>
-                    </aside>
-                  </section>
-                  <section className="section fr">
-                   <div className="subject__icon"></div>
-                   <div className="subject fc-4">
-                    <h3 className="subject__title fc-11"><a href="">Проценты, пропорции и дроби</a></h3>
-                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
-                  </div>
-                  <aside className="lessons fc-4 fr">
-                    <ul className="lessons__list">
-                      <li><a href="#">Введние в проценты</a></li>
-                      <li><a href="#">Введение в дроби</a></li>
-                      <li><a href="#">Сложение дробей</a></li>
-                    </ul>
-                    <ul className="lessons__list">
-                      <li><a href="#">Введние в проценты</a></li>
-                      <li><a href="#">Введение в дроби</a></li>
-                      <li><a href="#">Сложение дробей</a></li>
-                    </ul>
-                  </aside>
-                </section>*/}
-    </main>
-  </div>
-		);
+          <section className="section subject">
+            <div className="subject__group">
+              <div className="subject__icon"></div>
+              <a href="#" className="subject__title">Проценты, пропорции и дроби</a>
+            </div>
+            <div className="subject__container">
+              <p className="subject__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, eveniet reiciendis voluptates, maxime, quasi alias harum veniam animi ipsa unde non, adipisci ad dolorum dolorem. Rerum perferendis dolores exercitationem delectus ullam a error, libero reprehenderit molestiae corrupti in quasi, dolor ex. Vitae consequatur fugit porro, mollitia eveniet assumenda iste repellendus.</p>
+              <div className="subject__lessons">
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+                <div className="subject__lesson"><a href="#">Введение в степени</a></div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    );
 	}
 }
