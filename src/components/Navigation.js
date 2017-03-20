fbd4b06f5f0397afd558d36b087e80beb3228a27
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-20 16:01:02 +0400

    разбил блоки на компоненты

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+/*	смена страниц по нажатию таба	*/
+export default class Navigation extends Component {
+  render() {
+    return (
+    	<div className="navigation">
+	    	<a className="navigation__link navigation__link_active">Обзор курса</a>
+	    	<a className="navigation__link">Практикум</a>
+	    	<a className="navigation__link">Прогресс</a>
+      </div>
+    );
+  }
+}
