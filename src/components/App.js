b7b2efb8ed0df4d406191ad2c0e08d4bf0e34b8a
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-21 21:56:13 +0400

    получаем разделы с описанием и уроки к ним из бд

--- src/components/App.js
+++ src/components/App.js
@@ -8,52 +8,22 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: [
-        {
-          icon: 'img/percentage.png',
-          title: 'Проценты, пропорции и дроби',
-          description: 'Здесь будет супер-пупер крутое описание раздела',
-          lessons: [
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции'
-          ]
-        },
-        {
-          icon: 'img/natnumbers.png',
-          title: 'Натуральные числа и шкалы',
-          description: 'Здесь будет супер-пупер крутое описание раздела',
-          lessons: [
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции',
-            'Ввдение в степени',
-            'Ввдение в пропорции'
-          ]
-        }
-      ]
+      articles: []
     }
   }
 
-  // componentDidMount() {
-  //   fetch('articles.json')
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //     this.setState({
-  //       articles: data 
-  //     });
-  //   })
-  //   .catch(alert);
-  // }
+  componentDidMount() {
+    fetch('http://localhost:2403/articles')
+    .then(response => response.json())
+    .then(articles => {
+      this.setState({
+        articles
+      });
+    })
+    .catch(err => {
+      throw new Error('bad fetch');
+    });
+  }
   
   render() {
 		return (
