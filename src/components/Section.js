65b1fa3c56c5152dff0b1fe7c4319e1d02f76ca4
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-21 21:59:15 +0400

    убрал лишнию проверку

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,9 +3,6 @@ import Article from './Article';
 
 export default class Section extends Component {
 	render() {
-    if (this.props.data.length === 0)
-      return null;
-
 		let articles = this.props.data.map((article) => 
 			<Article data={article} key={article.id}/>
 		);
