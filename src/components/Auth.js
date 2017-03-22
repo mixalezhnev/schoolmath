325cb311586666398ce117df4e9f373b17e6cc57
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-22 21:10:51 +0400

    tabulated again

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,21 +1,19 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
 export default class Auth extends Component {
   render() {
-  	let fragment;
+    let fragment;
 
-  	if (this.props.isLogged) {
-  		fragment =
-  			<div className="profile">
-  				<a href="#" className="profile__link">Профиль</a>
-  			</div>;  		
-  	} else {
-  		fragment =     	
-  			<div className="sign-in">
-    			<a href="#" className="sign-in__link">Войти</a>
-    		</div>;
-  	}
-    
+    if (this.props.isLogged) {
+      fragment = <div className="profile">
+        <a href="#" className="profile__link">Профиль</a>
+      </div>;
+    } else {
+      fragment = <div className="sign-in">
+        <a href="#" className="sign-in__link">Войти</a>
+      </div>;
+    }
+
     return fragment;
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import '../public/styles/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(< App / >, document.getElementById('root'));
