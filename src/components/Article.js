24af0f8390e097968724bb8b32e1b244f7e67610
Mikhail Lezhnev <mixa.lezhnev@yandex.ru>
2017-03-31 01:39:16 +0400

    черновик просмотра уроков, пока все плохо

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Article(props) {
-  const {icon, title, description, lessons} = props.data;
+export default class Article extends Component {
 
-  return (
-    <div className='section subject'>
-      <div className='subject__group'>
-        <div className='subject__icon'></div>
-        <a href='#' className='subject__title'>{title}</a>
-      </div>
-      <div className='subject__container'>
-        <p className='subject__desc'>{description}</p>
-        <div className='subject__lessons'>
-          {lessons.map((lesson, index) => <div className='subject__lesson' key={index}>
-            <a href='#'>{lesson}</a>
-          </div>)}
-        </div>
-      </div>
-    </div>
-  );
+  onLinkClick = (e) => {
+    e.preventDefault();
+    
+    window.ee.emit('openLesson', e.target.textContent);
+  }
+
+  render() {
+    const {icon, title, description, lessons} = this.props.data;
+    return (
+    <div className = 'section subject' > <div className='subject__group'>
+      <div className='subject__icon'></div>
+      <a href='' className='subject__title'>{title}</a>
+    </div> < div className = 'subject__container' > <p className='subject__desc'>{description}</p> < div className = 'subject__lessons' > {
+      lessons.map((lesson, index) => <div className='subject__lesson' key={index}>
+        <a href='' onClick={this.onLinkClick}>{lesson.title}</a>
+      </div>)
+    } < /div>
+      </div > </div>);
+  }
 }
