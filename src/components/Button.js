de3bf153b5a7d390c12421efaed027f42d16c956
Alexander <pyxarez@gmail.com>
2017-03-31 16:33:17 +0400

    stateless components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-const Button = (props) => {
-  const {className, onClick} = props;
-
-  return (<div className={className} onClick={onClick}></div>
-  );
-}
+const Button = ({className, onClick}) => (
+  <div className={className} onClick={onClick}/>
+);
 
 export default Button;
