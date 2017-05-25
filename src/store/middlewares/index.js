c217d2c67b870b71afe8560814d0842c6a8069c7
Mikhail <mixa.lezhnev@gmail.com>
2017-05-25 20:27:30 +0400

    Update index.js
    omg... beautified

--- src/store/middlewares/index.js
+++ src/store/middlewares/index.js
@@ -1,22 +1,23 @@
 import { SYNC_EXERCISES, POST_GET_EXERCISE_SUCCESS }
 from '../constants/practice';
 
-const syncExercises = (exercises) => ({
-	type: SYNC_EXERCISES,
-	payload: exercises
-}
+const syncExercises = (exercises) => (
+	{
+		type: SYNC_EXERCISES,
+    payload: exercises
+  }
 )
 
 export const processExercises = ({ dispatch, getState }) => next => action => {
-	if (action.type == POST_GET_EXERCISE_SUCCESS) {
-		const state = getState();
+  if (action.type == POST_GET_EXERCISE_SUCCESS) {
+    const state = getState();
 		const { subject, lesson } = action.payload;
 		const exercises = state.progress.data.subjects[subject][lesson];
 
-		if (!exercises.completed){
-			dispatch(syncExercises(exercises))
+		if (!exercises.completed) {
+	  	dispatch(syncExercises(exercises))
 		}
-	}
+  }
 
-	return next(action);
+  return next(action);
 }
